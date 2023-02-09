ed9eb005a2fc7d7a23517c64a78166c9b080243b
hoang31201 <13743808@student.uts.edu.au>
2023-02-09 22:14:34 +0700

    Write Arrow Functions with Parameters

--- JavaScript Algorithms and Data Structures/test.js
+++ JavaScript Algorithms and Data Structures/test.js
@@ -43,3 +43,12 @@ var magic = function () {
 
 const magic = () => new Date();
 
+var myConcat = function (arr1, arr2) {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));

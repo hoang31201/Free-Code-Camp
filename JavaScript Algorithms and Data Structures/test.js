5e50777f6b47c1f92e8336dfd9f73584dafdd6f5
hoang31201 <13743808@student.uts.edu.au>
2023-02-12 20:38:47 +0700

    Use Destructuring Assignment to Extract Values from Objects

--- JavaScript Algorithms and Data Structures/test.js
+++ JavaScript Algorithms and Data Structures/test.js
@@ -92,3 +92,28 @@ const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
 const arr2 = [...arr1]; // Change this line
 
 console.log(arr2);
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const today = HIGH_TEMPERATURES.today;
+const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+// Only change code above this line
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line~

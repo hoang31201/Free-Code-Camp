b16a199ddda438234b1b291d23863a898ba0ce0e
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 15:20:30 +0700

    Use getters and setters to Control Access to an Object

--- JavaScript Algorithms and Data Structures/test.js
+++ JavaScript Algorithms and Data Structures/test.js
@@ -305,3 +305,24 @@ class Vegetable {
 
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Should display 'carrot'
+
+// Only change code below this line
+class Thermostat {
+  constructor(temp) {
+    this._temp = temp;
+  }
+  //getter
+  get temperature() {
+    return 5/9 * (this._temp - 32);
+  }
+  //setter
+  set temperature(updatedTemp) {
+    this._temp = updatedTemp * 9.0 / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius

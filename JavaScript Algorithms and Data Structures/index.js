9f55275a53f39fc7f233d9a72fb16efda8097e0d
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 15:45:07 +0700

    Import a Default Export

--- JavaScript Algorithms and Data Structures/index.js
+++ JavaScript Algorithms and Data Structures/index.js
@@ -354,3 +354,8 @@ function subtract(x, y) {
 export default function(x, y) {
   return x - y;
 }
+
+import subtract from "./math_functions.js";  
+// Only change code above this line
+
+subtract(7,4);

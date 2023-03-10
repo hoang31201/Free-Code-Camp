9028606ba3f91e99c477b36325f9c78a00d8c705
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 16:22:28 +0700

    Find More Than the First Match

--- JavaScript Algorithms and Data Structures/regex.js
+++ JavaScript Algorithms and Data Structures/regex.js
@@ -33,3 +33,11 @@ let result = extractStr; // Change this line
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /change/; // Change this line
+let result = twinkleStar; // Change this line
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line

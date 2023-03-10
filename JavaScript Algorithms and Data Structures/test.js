aff642943d06e10acfc3dca2ab9322e3d3d236cf
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 14:27:44 +0700

    Destructuring via rest elements

--- JavaScript Algorithms and Data Structures/test.js
+++ JavaScript Algorithms and Data Structures/test.js
@@ -173,3 +173,25 @@ let a = 8, b = 6;
 // Only change code below this line
 [a, b] = [b, a];
 console.log(a, b);
+
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const shorterList = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a, b, ...shorterList] = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+console.log(sourceWithoutFirstTwo);

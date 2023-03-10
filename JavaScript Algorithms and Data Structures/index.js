f93cb79ded3179bf7f1bb8c53cae13a821f39d0a
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 16:03:08 +0700

    Complete a Promise with resolve and reject

--- JavaScript Algorithms and Data Structures/index.js
+++ JavaScript Algorithms and Data Structures/index.js
@@ -363,3 +363,27 @@ subtract(7,4);
 const makeServerRequest = new Promise((resolve, reject) => {
 
 });
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    // Change this line
+  } else {  
+    // Change this line
+  }
+});
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    // Change this line
+    resolve("We got the data");
+  } else {  
+    // Change this line
+    reject("Data not received");
+  }
+});

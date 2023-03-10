eb1b42f774cdbc8e8e046de4a52e0b8db71e8838
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 14:42:45 +0700

    Use Destructuring Assignment to Pass an Object as a Function's Parameters

--- JavaScript Algorithms and Data Structures/test.js
+++ JavaScript Algorithms and Data Structures/test.js
@@ -195,3 +195,16 @@ const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source);
 
 console.log(sourceWithoutFirstTwo);
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = (stats) => (stats.max + stats.min) / 2.0; 
+// Only change code above this line

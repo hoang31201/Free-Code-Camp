44f3b2ab7ec8d1703ae854daa0a36b09738a3f9c
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 16:08:31 +0700

    Handle a Rejected Promise with catch

--- JavaScript Algorithms and Data Structures/index.js
+++ JavaScript Algorithms and Data Structures/index.js
@@ -398,4 +398,6 @@ const makeServerRequest = new Promise((resolve, reject) => {
   makeServerRequest.then((result) => {
     console.log(result);
   });
+  makeServerRequest.catch(error => {
+    console.log(error);
 });

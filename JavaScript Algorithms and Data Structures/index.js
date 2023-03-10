9c36aca6aabbb260ad66b8d3efb6f4b81df37739
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 15:33:42 +0700

    Use * to Import Everything from a File

--- JavaScript Algorithms and Data Structures/index.js
+++ JavaScript Algorithms and Data Structures/index.js
@@ -340,3 +340,9 @@ import { uppercaseString, lowercaseString } from './string_functions.js';
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");

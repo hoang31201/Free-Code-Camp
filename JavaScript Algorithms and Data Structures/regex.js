b4f841d5ffa49485199b56b46bb4a5f3af3e2c04
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 16:19:17 +0700

    Ignore Case While Matching

--- JavaScript Algorithms and Data Structures/regex.js
+++ JavaScript Algorithms and Data Structures/regex.js
@@ -17,3 +17,11 @@ let result = petRegex.test(petString);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+
+let myString = "freeCodeCamp";
+let fccRegex = /change/; // Change this line
+let result = fccRegex.test(myString);
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);

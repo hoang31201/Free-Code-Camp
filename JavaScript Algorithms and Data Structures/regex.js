df4bbd3dd6f201f2cd0cc3c5a519e8462f4329d9
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 16:21:08 +0700

    Extract Matches

--- JavaScript Algorithms and Data Structures/regex.js
+++ JavaScript Algorithms and Data Structures/regex.js
@@ -25,3 +25,11 @@ let result = fccRegex.test(myString);
 let myString = "freeCodeCamp";
 let fccRegex = /freecodecamp/i; // Change this line
 let result = fccRegex.test(myString);
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /change/; // Change this line
+let result = extractStr; // Change this line
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line

a86a6a110d51e2074919ab24f9c0e7ed02f63460
hoang31201 <13743808@student.uts.edu.au>
2023-03-10 16:18:21 +0700

    Match a Literal String with Different Possibilities

--- JavaScript Algorithms and Data Structures/regex.js
+++ JavaScript Algorithms and Data Structures/regex.js
@@ -9,3 +9,11 @@ let result = waldoRegex.test(waldoIsHiding);
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
+
+let petString = "James has a pet cat.";
+let petRegex = /change/; // Change this line
+let result = petRegex.test(petString);
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);

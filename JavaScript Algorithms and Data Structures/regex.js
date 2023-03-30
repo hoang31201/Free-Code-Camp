622c674c3ae935c0618d248095842dbf0a4180c8
hoang31201 <13743808@student.uts.edu.au>
2023-03-30 13:51:25 +0700

    Find One or More Criminals in a Hunt

--- JavaScript Algorithms and Data Structures/regex.js
+++ JavaScript Algorithms and Data Structures/regex.js
@@ -99,3 +99,7 @@ let result = text.match(myRegex);
 let text = "<h1>Winter is coming</h1>";
 let myRegex = /<.*?>/; // Change this line
 let result = text.match(myRegex);
+
+let reCriminals = /./; // Change this line
+
+let reCriminals = /C+/; // Change this line

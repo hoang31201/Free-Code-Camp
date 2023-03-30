0422da041ac9e1f04884b7f539f323ecff89a321
hoang31201 <13743808@student.uts.edu.au>
2023-03-30 13:37:41 +0700

    Match Characters that Occur One or More Times

--- JavaScript Algorithms and Data Structures/regex.js
+++ JavaScript Algorithms and Data Structures/regex.js
@@ -81,3 +81,7 @@ let result = myRegex; // Change this line
 let quoteSample = "3 blind mice.";
 let myRegex = /[^aeiou0-9]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
+
+let difficultSpelling = "Mississippi";
+let myRegex = /change/; // Change this line
+let result = difficultSpelling.match(myRegex);
